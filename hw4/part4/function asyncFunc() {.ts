6261bd0f51f2f52cb698fb9fa57ec18446667a53
David Shmailov <shmailov.david@gmail.com>
2021-02-19 13:47:41 +0200

    Add files via upload

---
+++ hw4/part4/function asyncFunc() {.ts
@@ -0,0 +1,9 @@
+// function asyncFunc() {
+//     return new Promise((resolve, reject) => { // (A)
+//         setTimeout(() => resolve('DONE'), 100); // (B)
+//     });
+// }
+// asyncFunc()
+// .then(x => console.log('Result: '+x));
+
+console.log('asdas')
